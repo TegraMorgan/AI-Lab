d0bdc306349b78fdea40e0c673bf6e45cffcddb8
Tomer Wolberg <tomer9074@gmail.com>
2019-04-06 15:44:49 +0300

    N queens work for small Ns (<500) but get stuck when n = 1000, different constants work better for different Ns

--- src/com/AILab3/Entities/QueensBrain.java
+++ src/com/AILab3/Entities/QueensBrain.java
@@ -1,52 +1,45 @@
 package com.AILab3.Entities;
 
-import java.awt.*;
 import java.util.Arrays;
 import java.util.Random;
 import java.lang.Math;
+import java.util.Vector;
 
+@SuppressWarnings("WeakerAccess")
 public class QueensBrain
 {
-    //On average 1 out of MUTATE_RANGE queens would be mutated.
-    public final static int MUTATE_RANGE = 10000;
     private static Random random = new Random();
     private float fitness;
-    private final int N;
-    private Point[] queens;
-    private boolean[] board;
+    private int[] queens;
+
+    //On average on each mutate() call n * MUTATION_PROBABILITY queens would be mutated
+    public static final float MUTATION_PROBABILITY = 0.01f;
+
+    //On average on each crossover() call n * MUTATION_PROBABILITY queens would be PMX swapped
+    public static final float CROSSOVER_PROBABILITY = 0.5f;
 
     public QueensBrain (int n)
     {
-        queens = new Point[n];
-        board = new boolean[n * n];
-        N = n;
-        Arrays.setAll(queens, i -> randomQueen());
-        fitness = calculateFitness();
-        for (Point queen : queens)
+        queens = new int[n];
+        Arrays.setAll(queens, i -> i);
+        for (int i = 0; i < n; i++)
         {
-            board[queen.y * n + queen.x] = true;
+            swap(i, i + random.nextInt((n - i)));
         }
+        calculateFitness();
     }
 
-    private Point randomQueen ()
+    private void swap (int q1, int q2)
     {
-        int x, y;
-        do
-        {
-            x = random.nextInt(N);
-            y = random.nextInt(N);
-        } while (board[y * N + x]);
-        board[y * N + x] = true;
-        return new Point(x, y);
+        int temp = queens[q1];
+        queens[q1] = queens[q2];
+        queens[q2] = temp;
     }
 
     public QueensBrain (QueensBrain b)
     {
         fitness = b.fitness;
-        N = b.N;
-        queens = new Point[b.queens.length];
-        Arrays.setAll(queens, i -> new Point(b.queens[i]));
-        board = b.board.clone();
+        queens = b.queens.clone();
     }
 
     public float getFitness ()
@@ -54,95 +47,96 @@ public class QueensBrain
         return fitness;
     }
 
-    private boolean threaten (Point q1, Point q2)
-    {
-        return q1.x == q2.x || q1.y == q2.y || Math.abs(q1.x - q2.x) == Math.abs(q1.y - q2.y);
-    }
-
     @Override
     public String toString ()
     {
-        String[] board = new String[N * N];
-        Arrays.fill(board, "[ ]");
-        for (Point p : queens)
-        {
-            board[p.y * N + p.x] = "[Q]";
-        }
         String str = "";
-        for (int i = 0; i < N; i++)
+        for (int y = 0; y < queens.length; y++)
         {
-            for (int j = 0; j < N; j++)
+            for (int x = 0; x < queens.length; x++)
             {
-                str += board[i * N + j];
+                str += queens[x] == y ? "[Q]" : "[ ]";
             }
             str += "\n";
         }
         return str;
     }
 
-    private float calculateFitness ()
+    private boolean threaten (int i1, int i2)
+    {
+        return queens[i1] == queens[i2] || Math.abs(i1 - i2) == Math.abs(queens[i1] - queens[i2]);
+    }
+
+    private void calculateFitness ()
     {
         int count = 0;
         for (int i = 0; i < queens.length; i++)
         {
             for (int j = i + 1; j < queens.length; j++)
             {
-                if (threaten(queens[i], queens[j]))
+                if (threaten(i, j))
                 {
                     count++;
                 }
             }
         }
-        return 1f / (float)(count + 1);
+        fitness = 1f / (count * count + 1f);
     }
 
     public boolean foundSolution ()
     {
-        return fitness == 1;
+        return fitness == 1f;
     }
 
     private QueensBrain mutate ()
     {
-        for (Point pos : queens)
+        Vector<Integer> shuffledIndices = new Vector<>();
+        for (int i = 0; i < queens.length; i++)
         {
-            if (random.nextInt(MUTATE_RANGE) == 0)
+            if (random.nextFloat() < MUTATION_PROBABILITY)
             {
-                Point r = randomQueen();
-                board[pos.y * N + pos.x] = false;
-                pos.x = r.x;
-                pos.y = r.y;
+                shuffledIndices.add(i);
             }
         }
-        fitness = calculateFitness();
+        for (int i = 0; i < shuffledIndices.size(); i++)
+        {
+            int r = i + random.nextInt((shuffledIndices.size() - i));
+            swap(shuffledIndices.elementAt(i), shuffledIndices.elementAt(r));
+            int temp = shuffledIndices.elementAt(i);
+            shuffledIndices.setElementAt(shuffledIndices.elementAt(r), i);
+            shuffledIndices.setElementAt(temp, r);
+        }
+        calculateFitness();
         return this;
     }
 
-    public static QueensBrain getBaby (QueensBrain[] gen, float fitnessSum)
+    public static QueensBrain getBaby (QueensBrain p1, QueensBrain p2)
     {
-        QueensBrain p1 = getParent(gen, fitnessSum), p2;
-        while ((p2 = getParent(gen, fitnessSum)) == p1) ;
-        return new QueensBrain(p1).crossover(p2).mutate();
+        return (random.nextBoolean() ? crossover(p1, p2) : crossover(p2, p1)).mutate();
     }
 
-    private static QueensBrain getParent (QueensBrain[] gen, float fitnessSum)
+    private int indexOf (int x)
     {
-        float r = random.nextFloat() * fitnessSum, sum = gen[0].fitness;
-        int i = 0;
-        while (sum < r)
-        {
-            sum += gen[++i].fitness;
-        }
-        return gen[i];
+        int y = -1;
+        while (queens[++y] != x) ;
+        return y;
     }
 
-    private QueensBrain crossover (QueensBrain b)
+    private static QueensBrain crossover (QueensBrain b1, QueensBrain b2)
     {
-        int h = random.nextBoolean() ? N : N / 2;
-        for (int i = h - N / 2; i < h; i++)
+        b1 = new QueensBrain(b1);
+        Vector<Integer> crossovers = new Vector<>();
+        for (int i = 0; i < b1.queens.length; i++)
         {
-            queens[i].x = b.queens[i].x;
-            queens[i].y = b.queens[i].y;
+            if (random.nextFloat() < CROSSOVER_PROBABILITY)
+            {
+                crossovers.add(i);
+            }
         }
-        return this;
+        for (int c : crossovers)
+        {
+            b1.swap(c, b1.indexOf(b2.queens[c]));
+        }
+        return b1;
     }
 }
--- src/com/AILab3/Entities/QueensPopulation.java
+++ src/com/AILab3/Entities/QueensPopulation.java
@@ -1,40 +1,103 @@
 package com.AILab3.Entities;
 
 import java.util.Arrays;
+import java.util.Random;
 
 public class QueensPopulation
 {
-    public static final int GENERATION_SIZE = 10000;
+    public static final int GENERATION_SIZE = 30;
+    private static Random random = new Random();
     private final int N;
     private QueensBrain[] gen = new QueensBrain[GENERATION_SIZE];
+    private QueensBrain solution = null;
+    private int genCount;
 
-    public QueensPopulation (int n) { N = n; }
-
-    public String repopulateTillSolution ()
+    public QueensPopulation (int n)
     {
+        N = n;
         Arrays.setAll(gen, i -> new QueensBrain(N));
-        int genCount = 0;
-        QueensBrain solution = null;
-        while (solution == null)
-        {   //while no solution: repopulate
-            QueensBrain[] newGen = new QueensBrain[GENERATION_SIZE];
-            float sum = 0;
-            for (QueensBrain b : gen)
+        genCount = 1;
+    }
+
+    private double[] fitnessCumSum ()
+    {
+        double[] cumsum = new double[GENERATION_SIZE];
+        cumsum[0] = gen[0].getFitness();
+        for (int i = 1; i < GENERATION_SIZE; i++)
+        {
+            cumsum[i] = cumsum[i - 1] + gen[i].getFitness();
+        }
+        double sum = cumsum[GENERATION_SIZE - 1];
+        for (int i = 0; i < GENERATION_SIZE; i++)
+        {
+            cumsum[i] /= sum;
+        }
+        return cumsum;
+    }
+
+    private QueensBrain wheelOfFortune (double[] cumsum)
+    {
+        double r = random.nextDouble();
+        int i = -1;
+        while (cumsum[++i] < r) ;
+        return gen[i];
+    }
+
+    private QueensBrain child (double[] cumsum)
+    {
+        QueensBrain p1 = wheelOfFortune(cumsum), p2;
+        while ((p2 = wheelOfFortune(cumsum)) == p1) ;
+        return QueensBrain.getBaby(p1, p2);
+    }
+
+    public QueensBrain getSolution ()
+    {
+        if (solution == null)
+        {
+            for (QueensBrain qb : gen)
             {
-                sum += b.getFitness();
-            }
-            for (int i = 0; i < GENERATION_SIZE && solution == null; i++)
-            {
-                QueensBrain baby = QueensBrain.getBaby(gen, sum);
-                newGen[i] = baby;
-                if (baby.foundSolution())
+                if (qb.foundSolution())
                 {
-                    solution = baby;
+                    solution = qb;
+                    break;
                 }
             }
-            genCount++;
-            gen = newGen;
         }
-        return "Gen " + genCount + ":\n" + solution.toString();
+        return solution;
+    }
+
+    public int getGenerationCount ()
+    {
+        return genCount;
+    }
+
+    private QueensBrain best ()
+    {
+        QueensBrain best = gen[0];
+        for (QueensBrain q : gen)
+        {
+            if (q.getFitness() >= best.getFitness())
+            {
+                best = q;
+            }
+        }
+        return best;
+    }
+
+    public void repopulateTillSolution ()
+    {
+        genCount = 1;
+        QueensBrain[] newGen = new QueensBrain[GENERATION_SIZE];
+        while ((solution = getSolution()) == null)
+        {
+            double[] cumsum = fitnessCumSum();
+            Arrays.setAll(newGen, i -> i == 0 ? best() : child(cumsum));
+            gen = newGen;
+            genCount++;
+            if (genCount % 2 == 0)
+            {
+                System.out.println("Gen " + genCount + ", F = " + Math.sqrt(1f / best().getFitness() - 1));
+            }
+        }
     }
 }
