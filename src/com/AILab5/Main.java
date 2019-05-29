20e976b034681539d09a81247aa56c2501a5bfc2
Tegra Morgan <tegra.morgan.net@gmail.com>
2019-05-29 23:48:52 +0300

    Small code inlining
    Signed-off-by: Tegra Morgan <tegra.morgan.net@gmail.com>

--- src/com/AILab5/Main.java
+++ src/com/AILab5/Main.java
@@ -1,18 +1,19 @@
 package com.AILab5;
 
 import com.AILab5.Entity.ColorGraph;
-import com.AILab5.Entity.Solutions;
+
+import static com.AILab5.CspAlgo.Utility.parseProblem;
+import static com.AILab5.Entity.Solutions.backJumping;
 
 public class Main
 {
     public static void main (String[] args)
     {
-
         // Read and Parse problem
-        boolean[][] _gr = com.AILab5.CspAlgo.Utility.parseProblem(10);
-
+        boolean[][] _gr = parseProblem(10);
+        if (_gr == null) return;
         // Run algo to find answer
-        boolean solved = Solutions.backJumping(new ColorGraph(_gr, 10));
+        boolean solved = backJumping(new ColorGraph(_gr, 5));
 
         // Output
         if (solved) System.out.println("Solved");
